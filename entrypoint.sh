42913ad1b5b10821d8de7aa7c3c15c205472a005
ivanmarinoff <marinoffivan@gmail.com>
2024-10-24 10:03:12 +0300

    Add requirements

---
+++ entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+echo "Install the dependencies"
+pip install -r requirements.txt
+
+
+echo "Running Database Migrations"
+python manage.py makemigrations
+python manage.py migrate
+
+echo "Running app commands"
+python manage.py collectstatic --noinput
+python manage.py runserver &
+exec "$@"
